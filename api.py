0f535cbd376b2010617fc74d203016707215e37f
daifugu5246 <patipat.kd@gmail.com>
2025-02-21 14:12:54 +0700

    second commit

--- api.py
+++ api.py
@@ -1,13 +1,20 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsandBytesConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from peft import PeftModel
+import torch
 import time
 from fastapi import FastAPI
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+print("Device name:", torch.cuda.get_device_properties('cuda').name)
+print("FlashAttention available:", torch.backends.cuda.flash_sdp_enabled())
+print(f'torch version: {torch.version}')
+
 # load base model
 model_name = "KBTG-Labs/THaLLE-0.1-7B-fa"
-tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir="../../model-cache")
-base_model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir="../../model-cache", torch_dtype=torch.bfloat16)
+tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir="../model-cache")
+quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+base_model = AutoModelForCausalLM.from_pretrained(model_name, cache_dir="../model-cache", quantization_config=quantization_config)
 
 # load lora model
 adapter_model = './lora_model_cot'
@@ -22,11 +29,29 @@ Data:
 {}
 Event:
 {}
-###Retrived:
+###Retrieved:
 {}
 ###Output:
 
 '''
+
+prompt = 'PTT คือ บริษัทเกี่ยวกับอะไร?'
+
+inputs = tokenizer(prompt, return_tensors='pt').to(device)
+
+outputs = lora_model.generate(
+    inputs['input_ids'],
+    max_new_tokens=2048,
+    temperature = 0.8,
+    top_k = 50,
+    top_p = 0.8,
+    repetition_penalty = 1.1,
+    pad_token_id=tokenizer.eos_token_id
+)
+
+outputs = [output_ids[len(input_ids):] for input_ids, output_ids in zip(inputs.input_ids, outputs) ]
+response = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+print(response)
 
 # API
 app = FastAPI()
@@ -37,9 +62,6 @@ def read_root():
 
 @app.get("/generate")
 def generate(instruction: str, data: str, event: str):
-    '''
-    Retrieve more data from Vector Search
-    '''
     inputs = tokenizer(prompt, return_tensors="pt")
     outputs = lora_model.generate(
         inputs['input_ids'], 
@@ -48,6 +70,7 @@ def generate(instruction: str, data: str, event: str):
         top_k=50,            # การสุ่มจาก top_k โทเคน
         top_p=0.8,           # การสุ่มแบบ nucleus sampling
         repetition_penalty=1.1,
+
     )
     outputs = [
         output_ids[len(input_ids):] for input_ids, output_ids in zip(inputs.input_ids, outputs)
