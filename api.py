4214e726ca91b55d0b92767cd81c1a04b92fcbf4
daifugu5246 <patipat.kd@gmail.com>
2025-02-23 13:53:32 +0700

    update function retriever

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsandBytesConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from pydantic import BaseModel
 from peft import PeftModel
 import time
 from fastapi import FastAPI
@@ -16,6 +17,14 @@ lora_model = PeftModel.from_pretrained(base_model, adapter_model)
 # load model to device
 lora_model = lora_model.to(device)
 
+# load vectorstore
+vector_store = load_all_vector_stores()
+
+# mockup data
+symbol = "PTT"
+quarter ="Q1_66"
+
+
 format_prompt = '''###Instruction: {}
 ###Input: 
 Data:
@@ -28,6 +37,35 @@ Event:
 
 '''
 
+""" prompt = 'PTT คือ บริษัทเกี่ยวกับอะไร?'
+
+inputs = tokenizer(prompt, return_tensors='pt').to(device)
+
+outputs = lora_model.generate(
+    inputs['input_ids'],
+    max_new_tokens=2048,
+    temperature = 0.8,
+    top_k = 50,
+    top_p = 0.8,
+    repetition_penalty = 1.1,
+    pad_token_id=tokenizer.eos_token_id
+)
+
+outputs = [output_ids[len(input_ids):] for input_ids, output_ids in zip(inputs.input_ids, outputs) ]
+response = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+print(response) """
+
+# prompt
+class PromptModel(BaseModel):
+    instruction: str
+    data: str
+    event: str
+
+# ฝากแทนเขียน function นี้ด้วยนะครับ
+def retriever(retriever_prompt):
+    pass
+
+
 # API
 app = FastAPI()
 
@@ -35,12 +73,18 @@ app = FastAPI()
 def read_root():
     return {"message": "Finacial Articles Writing using Artificial Intelligence."}
 
-@app.get("/generate")
-def generate(instruction: str, data: str, event: str):
-    '''
-    Retrieve more data from Vector Search
-    '''
-    inputs = tokenizer(prompt, return_tensors="pt")
+@app.post("/generate")
+def generate(req: PromptModel):
+    
+    start = time.time()
+    
+    # retrieve data
+    retriever_prompt = format_retriever.format(req.instruction, req.data, req.event)
+    retrieved = retriever(retriever_prompt)
+
+    # generate
+    prompt = format_prompt.format(req.instruction, req.data, req.event, retrieved)
+    inputs = tokenizer(prompt, return_tensors="pt").to(device)
     outputs = lora_model.generate(
         inputs['input_ids'], 
         max_new_tokens=2048,  # คุณสามารถเปลี่ยนค่านี้เพื่อควบคุมความยาวของผลลัพธ์
